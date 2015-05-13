e6ff743fd25bedc75cf174872170977920d7a1b0
ViniciusFerreiraa <viniword@gmail.com>
2015-05-13 12:08:08 -0300

    Menu e pause

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void jogar(){
+		Application.LoadLevel ("Game");
+	}
+
+	public void creditos(){
+		
+	}
+
+	public void sair(){
+		Application.Quit ();
+	}
+}
