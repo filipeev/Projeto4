b3521b1b406f2c97e01b602958f72b1697b816ed
filipe <filipexokz@gmail.com>
2015-05-13 20:52:16 -0300

    mapa_viking

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -2,16 +2,19 @@
 using System.Collections;
 
 public class MoveController : MonoBehaviour {
+    public Animator anim;
     public Vector3 goPosition;
+    public Transform Arrow;
     public float speed;
     private Vector3 velocity = new Vector3(0.5f, 0.5f, 0.5f);
     private float smoothTime = 0.5f;
-    private bool moving = false;
+    public bool moving = false;
     public bool canMove, ableMove = true;
     private Quaternion targetRotation;
+    private Vector3 stayPosition;
 	// Use this for initialization
 	void Start () {
-        velocity = new Vector3(speed,speed,speed);
+        velocity = new Vector3(speed,0,speed);
 	}
 	
 	// Update is called once per frame
@@ -35,15 +38,44 @@ public class MoveController : MonoBehaviour {
                // targetRotation = Quaternion.LookRotation(goPosition - transform.position);
                 targetRotation.x = 0;
                 targetRotation.z = 0;
+                DestroyArrows();
+                Transform arrowClone = Instantiate(Arrow, new Vector3(goPosition.x, 2.73f, goPosition.z), Quaternion.Euler(0, 0, 0)) as Transform;
+                anim.SetFloat("posX", (goPosition.x - transform.position.x) );
+                anim.SetFloat("posY", (goPosition.z - transform.position.z) );
             }
         }
 
         if (moving)
         {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(goPosition.x, 0.50f, goPosition.z), ref velocity, smoothTime, 4);
-            //transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, .05f);
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(goPosition.x, 0.70f, goPosition.z), ref velocity, smoothTime, 4);
             transform.rotation = Quaternion.Euler(0, 0, 0);
-            ableMove = true; 
+        }       
+    }
+
+    void DestroyArrows()
+    {
+        Destroy(GameObject.FindWithTag("Arrow"));
+    }
+
+    void PlayerStop()
+    {
+       // moving = false;
+        anim.SetFloat("posX", 0);
+        anim.SetFloat("posY", 0);
+    }
+
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Arrow")
+        {
+            DestroyArrows();
+            PlayerStop();
         }
     }
+
+   void OnCollisionEnter(Collision other)
+   {
+       PlayerStop();
+   }
+
 }
